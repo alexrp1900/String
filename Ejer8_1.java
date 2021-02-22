cddff798e29cb1381da297f20d47258d0b844327
alexrp1900 <56952231+alexrp1900@users.noreply.github.com>
2021-02-22 10:26:08 +0100

    Add files via upload

---
+++ Ejer8_1.java
@@ -0,0 +1,17 @@
+package Prueba;
+
+import java.util.Scanner;
+
+public class Ejer8_1 {
+
+	public static void main(String[] args) {
+
+		
+		
+		String cadena = "En un lugar de la mancha de cuyo nombre no quiero acordarme";
+	
+		String[] palabras = cadena.split(" ");
+		
+		System.out.println("Tiene "+palabras.length+" palabras");
+	}
+}
